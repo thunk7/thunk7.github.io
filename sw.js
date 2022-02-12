e42e2578885b6e5fa5d399830ecac7df947ce664
thunk7 <thunk77@gmail.com>
2022-02-12 16:53:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,18 +2,9 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('v1').then(function(cache) {
             return cache.addAll([
-                '/Kavvadias_Project_3/uomTrack.html',
-                '/Kavvadias_Project_3/uomTrack.css',
-                '/Kavvadias_Project_3/uomTrack.js',
-                '/ServiceWorkers/assets/close.svg',
-                '/ServiceWorkers/assets/complete.svg',
-                '/ServiceWorkers/assets/delete.svg',
-                '/ServiceWorkers/assets/downArrow.svg',
-                '/ServiceWorkers/assets/edit.svg',
-                '/ServiceWorkers/assets/logo.svg',
-                '/ServiceWorkers/assets/reactivate.svg',
-                '/ServiceWorkers/assets/SourceSansPro.ttf',
-                '/ServiceWorkers/assets/upArrow.svg'
+                '/thunk7.github.io/uomTrack.html',
+                '/thunk7.github.io/uomTrack.css',
+                '/thunk7.github.io/uomTrack.js'
             ]);
         })
     );
