095a90883cb8937b41de8408dfff5e5ca2a69c47
thunk7 <thunk77@gmail.com>
2022-02-12 18:56:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function (event) {
     console.log('The service worker is being installed.');
     event.waitUntil(
-        caches.open('uomTrack').then(function(cache) {
+        caches.open('v1').then(function(cache) {
             return cache.addAll([
                 '/index.html',
                 '/uomTrack.js',
