2712751b903ce885e925481a7cc7fdcffa4e2701
thunk7 <thunk77@gmail.com>
2022-02-13 17:45:24 +0200

    Update uomTrack.js

--- uomTrack.js
+++ uomTrack.js
@@ -36,12 +36,9 @@ function reload() {
 reloadButton.addEventListener('click', reload, false);
 
 function addRow() {
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
+    var x = document.getElementById('uomTrack').tBodies[1];
+    var node = tBodies[1].cloneNode(true);
+    x.appendChild(node);
 }
 
 function cloneRow(n) {
