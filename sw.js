ea99de142551c382c1e535bb22c83b309625e830
thunk7 <thunk77@gmail.com>
2022-02-13 17:20:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,35 @@
-self.addEventListener('install', function (event) {
+this.addEventListener('install', function (event) {
     console.log('The service worker is being installed.');
     event.waitUntil(
-        caches.open('assets').then(function(cache) {
+        caches.open('v1').then(function(cache) {
             return cache.addAll([
                 '/index.html',
                 '/uomTrack.js',
                 '/uomTrack.css',
-                '/manifest.json'
+                '/manifest.json',
+                '/assets/ErrorMessage.png',
+                '/assets/table128.png',
+                '/assets/table512.png'
             ]);
         })
     );
 });
 
-// Runs whenever there is a fetch request
-self.addEventListener("fetch", (event) => {
-    // Checks the cache to find matching request. 
-    // If there is a cached version of the request, it returns the cached version, otherwise it makes a new request.
-    event.respondWith(
-        caches.match(event.request).then((res) => {
-            return res || fetch(event.request);
-        })
-    );
+this.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        let responseClone = response.clone();
+        caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/assets/ErrorMessage.png');
+      });
+    }
+  }));
 });
+
