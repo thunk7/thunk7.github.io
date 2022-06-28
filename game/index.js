7c62e4fee2a60274653e3b0fcf024a4f77a317e6
thunk7 <thunk77@gmail.com>
2022-06-28 14:51:09 +0300

    Update index.js

--- game/index.js
+++ game/index.js
@@ -7,3 +7,12 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('active');
     showcase.classList.toggle('active');
 })
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(res => console.log("service worker registered"))
+      .catch(err => console.log("service worker not registered", err))
+  })
+}
